ea488e96ca53e8cd220c5249ba480cf079496c0b
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-12-26 02:34:16 -0500

    Deploy

--- script/Harvest.s.sol
+++ script/Harvest.s.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.27;
 import {Script} from "forge-std/Script.sol";
 import {Harvest} from "../contracts/Harvest.sol";
 
+// source .env
+// forge script script/Harvest.s.sol:HarvestScript --chain mainnet --rpc-url ${RPC_URL} --broadcast --verify --etherscan-api-key ${ETHERSCAN_API_KEY} --account deployer
 contract HarvestScript is Script {
     Harvest public harvest;
 
